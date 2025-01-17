d6302190a117353f8eb356ede4b6a7e2b92253ad
Maximize0987 <42393700+Maximize0987@users.noreply.github.com>
2025-01-17 12:52:34 -0700

    Update interface.py. test tune

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -89,7 +89,7 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.init('pid')
       ret.lateralTuning.pid.kiBP = [0.0]
       ret.lateralTuning.pid.kpBP = [0.0]
-      ret.lateralTuning.pid.kpV = [0.50]
+      ret.lateralTuning.pid.kpV = [0.48]
       ret.lateralTuning.pid.kiV = [0.118]
       ret.lateralTuning.pid.kf = 0.00007818594
 
