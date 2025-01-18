c84908d851450c29ac5cad35bc78c4eaa23a57b4
Maximize0987 <42393700+Maximize0987@users.noreply.github.com>
2025-01-18 05:44:55 -0700

    Update interface.py.   test tune

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -90,7 +90,7 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kiBP = [0.0]
       ret.lateralTuning.pid.kpBP = [0.0]
       ret.lateralTuning.pid.kpV = [0.48]
-      ret.lateralTuning.pid.kiV = [0.118]
+      ret.lateralTuning.pid.kiV = [0.122]
       ret.lateralTuning.pid.kf = 0.00007818594
 
       # 2019+ RAV4 TSS2 uses two different steering racks and specific tuning seems to be necessary.
