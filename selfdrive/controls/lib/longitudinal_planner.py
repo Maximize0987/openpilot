3024203fd4d5d752a4a5f0d05110edc45655da1a
Maximize0987 <42393700+Maximize0987@users.noreply.github.com>
2025-09-02 19:39:47 -0600

    Update longitudinal_planner.py increase accel in curves

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -24,7 +24,7 @@ ALLOW_THROTTLE_THRESHOLD = 0.5
 MIN_ALLOW_THROTTLE_SPEED = 2.5
 
 # Lookup table for turns
-_A_TOTAL_MAX_V = [1.7, 3.2]
+_A_TOTAL_MAX_V = [1.7, 4.2]
 _A_TOTAL_MAX_BP = [20., 40.]
 
 
