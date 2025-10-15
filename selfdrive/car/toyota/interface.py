9a5fc546af781b1cafa885488e0b51eb8fb17d96
Maximize0987 <42393700+Maximize0987@users.noreply.github.com>
2025-10-15 12:39:43 -0600

    Update interface.py   test lower ki previous common error high was 46%

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -75,9 +75,9 @@ class CarInterface(CarInterfaceBase):
 
     elif candidate in (CAR.TOYOTA_RAV4_TSS2, CAR.TOYOTA_RAV4_TSS2_2022, CAR.TOYOTA_RAV4_TSS2_2023, CAR.TOYOTA_RAV4_PRIME, CAR.TOYOTA_SIENNA_4TH_GEN):
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
-      #ret.lateralTuning.torque.kp = 1.0
-      #ret.lateralTuning.torque.ki = 0.3
-      #ret.lateralTuning.torque.kf = 1.0
+      ret.lateralTuning.torque.kp = 1.0
+      ret.lateralTuning.torque.ki = 0.2
+      ret.lateralTuning.torque.kf = 1.0
       #ret.lateralTuning.init('pid')
       #ret.lateralTuning.pid.kiBP = [0.0]
       #ret.lateralTuning.pid.kpBP = [0.0]
