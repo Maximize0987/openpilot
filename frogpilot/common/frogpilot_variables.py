dcab628abb61d3110610712829ae3423824e605e
Maximize0987 <42393700+Maximize0987@users.noreply.github.com>
2025-10-26 15:17:30 -0600

    Update frogpilot_variables.py  attempt 1.0 kp

--- frogpilot/common/frogpilot_variables.py
+++ frogpilot/common/frogpilot_variables.py
@@ -397,7 +397,7 @@ frogpilot_default_params: list[tuple[str, str | bytes, int, str]] = [
   ("SteerFriction", "", 3, ""),
   ("SteerFrictionStock", "", 3, ""),
   ("SteerKP", "", 3, ""),
-  ("SteerKPStock", "", 3, ""),
+  ("SteerKPStock", "1.0", 3, ""),
   ("SteerLatAccel", "", 3, ""),
   ("SteerLatAccelStock", "", 3, ""),
   ("SteerRatio", "", 3, ""),
