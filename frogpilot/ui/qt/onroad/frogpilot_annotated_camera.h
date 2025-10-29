b34d1f7113ff0d9deadc10829ae2ff2f4f1d209f
Maximize0987 <42393700+Maximize0987@users.noreply.github.com>
2025-10-29 10:39:31 -0600

    Update frogpilot_annotated_camera.h   red2 added

--- frogpilot/ui/qt/onroad/frogpilot_annotated_camera.h
+++ frogpilot/ui/qt/onroad/frogpilot_annotated_camera.h
@@ -94,6 +94,7 @@ private:
   QColor blackColor(int alpha = 255) { return QColor(0, 0, 0, alpha); }
   QColor greenColor(int alpha = 242) { return QColor(23, 134, 68, alpha); }
   QColor redColor(int alpha = 255) { return QColor(201, 34, 49, alpha); }
+  QColor red2Color(int alpha = 255) { return QColor(255, 0, 0, alpha); }
   QColor whiteColor(int alpha = 255) { return QColor(255, 255, 255, alpha); }
   QColor orangeColor(int alpha = 255) { return QColor(255, 128, 0, alpha); }
   QColor yellowColor(int alpha = 255) { return QColor(255, 255, 0, alpha); }
