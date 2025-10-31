452cc4a5f1846359685e9029d255f50e89e6c6c3
Maximize0987 <42393700+Maximize0987@users.noreply.github.com>
2025-10-31 14:42:28 -0600

    Update latcontrol_torque.py. nudge teeak

--- selfdrive/controls/lib/latcontrol_torque.py
+++ selfdrive/controls/lib/latcontrol_torque.py
@@ -29,7 +29,7 @@ from openpilot.selfdrive.controls.lib.vehicle_model import ACCELERATION_DUE_TO_G
 
 LL_CLOSE = 1.8
 NUDGE_INPUT = [-1.55, -0.1, -0.05, 0, 1.45]
-NUDGE_OUTPUT = [-0.2, -0.04, 0, 0.04, 0.2]
+NUDGE_OUTPUT = [-0.2, -0.025, 0, 0.025, 0.2]
 #NUDGE_INPUT = [-0.3, -0.25, 0, 0.2, 0.201]
 #NUDGE_OUTPUT = [0, 0, 0, 0, 0.18]
 
