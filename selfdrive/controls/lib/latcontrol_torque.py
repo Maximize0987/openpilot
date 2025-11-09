92dbd938fb8642fb581ac247f1a8c96ccaa3b9db
Maximize0987 <42393700+Maximize0987@users.noreply.github.com>
2025-11-09 07:39:53 -0700

    Update latcontrol_torque.py. change print

--- selfdrive/controls/lib/latcontrol_torque.py
+++ selfdrive/controls/lib/latcontrol_torque.py
@@ -134,7 +134,7 @@ class LatControlTorque(LatControl):
         future_desired_lateral_accel += lane_val
         self.last_nudge = lane_val
       if abs(fdla1) > 0.8 and not nudge_off:
-        fdla4 = fdla3 / fdla1
+        fdla4 = fdla3 / fdla2
         fdla4 = round(fdla4, 3)
         lane_avg = round(lane_avg, 3)
         print(f"1: {fdla1} 2: {fdla2} 3: {fdla3} 4: {lane_avg} 5: {fdla4}")
