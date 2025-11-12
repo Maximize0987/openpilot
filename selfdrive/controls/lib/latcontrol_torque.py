c0ebabffe51303efc8b3079dbfd7b0f54e8acf36
Maximize0987 <42393700+Maximize0987@users.noreply.github.com>
2025-11-11 17:18:48 -0700

    Update latcontrol_torque.py. more kf tweak

--- selfdrive/controls/lib/latcontrol_torque.py
+++ selfdrive/controls/lib/latcontrol_torque.py
@@ -36,14 +36,14 @@ NUDGE_OUTPUT = [-0.08, -0.02, 0, 0.02, 0.08]
 
 KF_INPUT = [-1, -0.1, 0, 0.1, 1]
 KF_LC = [1.14, 1.08, 1.05, 1, 0.94]
-KF_RC = [0.94, 1, 1.05, 1.08, 1.14]
+KF_RC = [0.92, 1, 1.06, 1.09, 1.2]
 #KF_INPUT = [0, 10, 14]
 #KF_OUTPUT = [1.1, 1, 0.9775]
 
 KP = 1.0
 KI = 0.3
 KD = 0.0
-KF = 0.95    # default base for curvature corrrection used in line 118
+KF = 0.952    # default base for curvature corrrection used in line 118
 
 INTERP_SPEEDS = [1, 1.5, 2.0, 3.0, 5, 7.5, 10, 15, 30]
 KP_INTERP = [250, 120, 65, 30, 11.5, 5.5, 3.5, 2.0, KP]
