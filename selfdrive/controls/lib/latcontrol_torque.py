6341b1d711fe152e14eede9b6d6db71e1c7195fa
Maximize0987 <42393700+Maximize0987@users.noreply.github.com>
2025-11-20 18:40:40 -0700

    Update latcontrol_torque.py.  lower beginning kf to .95

--- selfdrive/controls/lib/latcontrol_torque.py
+++ selfdrive/controls/lib/latcontrol_torque.py
@@ -39,7 +39,7 @@ KF_RC = [0.95, 1, 1.05]
 KP = 1.0
 KI = 0.3
 KD = 0.0
-KF = 0.955    # default base for curvature corrrection
+KF = 0.95    # default base for curvature corrrection
 
 INTERP_SPEEDS = [1, 1.5, 2.0, 3.0, 5, 7.5, 10, 15, 30]
 #KP_INTERP = [250, 120, 65, 30, 11.5, 5.5, 3.5, 2.0, KP]
