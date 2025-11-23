a73911a29114f213618c858ad8765887798ed53e
Maximize0987 <42393700+Maximize0987@users.noreply.github.com>
2025-11-23 11:53:36 -0700

    Update latcontrol_torque.py  static 0.96 KF

--- selfdrive/controls/lib/latcontrol_torque.py
+++ selfdrive/controls/lib/latcontrol_torque.py
@@ -33,13 +33,13 @@ NUDGE_INPUT = [-1.58, -0.23, -0.08, 0.07, 1.42]
 NUDGE_OUTPUT = [-0.08, -0.02, 0, 0.02, 0.08]
 
 KF_INPUT = [-1.08, -0.08, 0.92]
-KF_LC = [1.055, 1, 0.96]
-KF_RC = [0.96, 1, 1.055]
+KF_LC = [1.06, 1, 0.955]
+KF_RC = [0.955, 1, 1.06]
 
 KP = 1.0
 KI = 0.3
 KD = 0.0
-KF = 0.95    # default base for curvature corrrection
+KF = 0.96    # default base for curvature corrrection
 
 INTERP_SPEEDS = [1, 1.5, 2.0, 3.0, 5, 7.5, 10, 15, 30]
 #KP_INTERP = [250, 120, 65, 30, 11.5, 5.5, 3.5, 2.0, KP]
@@ -143,7 +143,7 @@ class LatControlTorque(LatControl):
           self.avg_rkf = avg_kf
           self.rightcycles = 0
           self.total_rkf = 0
-          self.kf_live = (self.avg_rkf + self.avg_lkf) / 2
+          #self.kf_live = (self.avg_rkf + self.avg_lkf) / 2
       elif future_desired_lateral_accel < -0.1 and not kf_off: 
         fdla = interp(lane_avg, KF_INPUT, KF_LC)
         future_desired_lateral_accel *= fdla
@@ -159,7 +159,7 @@ class LatControlTorque(LatControl):
           self.avg_lkf = avg_kf
           self.leftcycles = 0
           self.total_lkf = 0
-          self.kf_live = (self.avg_rkf + self.avg_lkf) / 2
+          #self.kf_live = (self.avg_rkf + self.avg_lkf) / 2
       if right_lane > left_lane < LL_CLOSE or left_lane > right_lane < LL_CLOSE and CS.vEgo > 22 and not nudge_off:
         future_desired_lateral_accel += lane_val
         self.last_nudge = lane_val
