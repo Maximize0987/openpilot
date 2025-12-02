a7268996b8590d2eb28dea27a95e4d571324403a
Maximize0987 <42393700+Maximize0987@users.noreply.github.com>
2025-12-02 07:41:46 -0700

    Update ui.cc. clip brightness line 429. 10 to 5

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -426,7 +426,7 @@ void Device::updateBrightness(const UIState &s, const FrogPilotUIState &fs) {
     }
 
     // Scale back to 10% to 100%
-    clipped_brightness = std::clamp(100.0f * clipped_brightness, 10.0f, 100.0f);
+    clipped_brightness = std::clamp(100.0f * clipped_brightness, 5.0f, 100.0f);
   }
 
   int brightness = brightness_filter.update(clipped_brightness);
