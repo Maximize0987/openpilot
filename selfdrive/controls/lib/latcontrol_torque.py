c02e05580ca5c33d8f3225b4b40e5d079e185bd8
Maximize0987 <42393700+Maximize0987@users.noreply.github.com>
2025-12-09 13:00:01 -0700

    Update latcontrol_torque.py.  reenable base kf

--- selfdrive/controls/lib/latcontrol_torque.py
+++ selfdrive/controls/lib/latcontrol_torque.py
@@ -118,7 +118,7 @@ class LatControlTorque(LatControl):
       fdla3 = 1
       fdla4 = 1
       fdla1 = round(future_desired_lateral_accel, 3)      
-      #future_desired_lateral_accel *= KF
+      future_desired_lateral_accel *= KF
       fdla2 = round(future_desired_lateral_accel, 3)
       if future_desired_lateral_accel > 0 and not kf_off:
         fdla = interp(lane_avg, LANE_IN, KF_RC)
