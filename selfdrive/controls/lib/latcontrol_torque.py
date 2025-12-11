2eeb7106d18bc2970aa4f8ac2300493ff8a57f16
Maximize0987 <42393700+Maximize0987@users.noreply.github.com>
2025-12-10 17:49:44 -0700

    Update latcontrol_torque.py   disable print to see more important ROLL print

--- selfdrive/controls/lib/latcontrol_torque.py
+++ selfdrive/controls/lib/latcontrol_torque.py
@@ -162,7 +162,7 @@ class LatControlTorque(LatControl):
         avg = round(self.kf_live, 3)
         lane_val = round(lane_val, 3)
         roll_comp = round(roll_compensation, 4)
-        print(f"NUD: {lane_val} CV: {fdla4} AVG: {avg} Roll: {roll_comp}")
+        #print(f"NUD: {lane_val} CV: {fdla4} AVG: {avg} Roll: {roll_comp}")
       # end lane position data   
       self.lat_accel_request_buffer.append(future_desired_lateral_accel)
       gravity_adjusted_future_lateral_accel = future_desired_lateral_accel - roll_compensation
