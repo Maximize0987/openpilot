dce9421e06e1a9cd0155b7318827dee410922267
Maximize0987 <42393700+Maximize0987@users.noreply.github.com>
2026-04-07 18:36:52 -0600

    Update settings.cc. reset lparams

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -233,7 +233,7 @@ DevicePanel::DevicePanel(SettingsWindow *parent) : ListWidget(parent) {
     if (ConfirmationDialog::confirm(tr("Are you sure you want to reset calibration?"), tr("Reset"), this)) {
       params.remove("CalibrationParams");
       params.remove("LiveTorqueParameters");
-      //params.remove("LiveParameters");
+      params.remove("LiveParameters");
       //params.remove("LiveDelay");
     }
   });
