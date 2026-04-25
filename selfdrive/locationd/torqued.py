927267e267f37edb081225603bd69214880872fe
Maximize0987 <42393700+Maximize0987@users.noreply.github.com>
2026-04-25 12:39:25 -0600

    Update torqued.py. reduce allowed range friction and lat_accel

--- selfdrive/locationd/torqued.py
+++ selfdrive/locationd/torqued.py
@@ -87,10 +87,10 @@ class TorqueEstimator(ParameterEstimator):
       'points': []
     }
     self.decay = MIN_FILTER_DECAY
-    self.min_lataccel_factor = 2.1      #    self.min_lataccel_factor = (1.0 - self.factor_sanity) * self.offline_latAccelFactor
-    self.max_lataccel_factor = 2.8      #    self.max_lataccel_factor = (1.0 + self.factor_sanity) * self.offline_latAccelFactor
-    self.min_friction = 0.075                 #    self.min_friction = (1.0 - self.friction_sanity) * self.offline_friction
-    self.max_friction = 0.14                 #    self.max_friction = (1.0 + self.friction_sanity) * self.offline_friction
+    self.min_lataccel_factor = 2.49      #    self.min_lataccel_factor = (1.0 - self.factor_sanity) * self.offline_latAccelFactor
+    self.max_lataccel_factor = 2.51      #    self.max_lataccel_factor = (1.0 + self.factor_sanity) * self.offline_latAccelFactor
+    self.min_friction = 0.09                 #    self.min_friction = (1.0 - self.friction_sanity) * self.offline_friction
+    self.max_friction = 0.11                 #    self.max_friction = (1.0 + self.friction_sanity) * self.offline_friction
 
     # try to restore cached params
     params = Params()
