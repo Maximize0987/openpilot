158c65ac90bcda22f1430e497b5af84ab59ccb67
Maximize0987 <42393700+Maximize0987@users.noreply.github.com>
2026-05-21 20:12:55 -0600

    Update torqued.py.  more range

--- selfdrive/locationd/torqued.py
+++ selfdrive/locationd/torqued.py
@@ -87,8 +87,8 @@ class TorqueEstimator(ParameterEstimator):
       'points': []
     }
     self.decay = MIN_FILTER_DECAY
-    self.min_lataccel_factor = 1.8      #    self.min_lataccel_factor = (1.0 - self.factor_sanity) * self.offline_latAccelFactor
-    self.max_lataccel_factor = 2.8      #    self.max_lataccel_factor = (1.0 + self.factor_sanity) * self.offline_latAccelFactor
+    self.min_lataccel_factor = 1.5      #    self.min_lataccel_factor = (1.0 - self.factor_sanity) * self.offline_latAccelFactor
+    self.max_lataccel_factor = 3.0     #    self.max_lataccel_factor = (1.0 + self.factor_sanity) * self.offline_latAccelFactor
     self.min_friction = 0.07                 #    self.min_friction = (1.0 - self.friction_sanity) * self.offline_friction
     self.max_friction = 0.18                #    self.max_friction = (1.0 + self.friction_sanity) * self.offline_friction
 
