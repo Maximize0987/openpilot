588918bd106803f3553a2d51236cbb3899fe3afd
Maximize0987 <42393700+Maximize0987@users.noreply.github.com>
2026-06-18 07:04:56 -0700

    Update radar_interface.py.  add radar offset part2
    Signed-off-by: Maximize0987 <42393700+Maximize0987@users.noreply.github.com>

--- opendbc_repo/opendbc/car/toyota/radar_interface.py
+++ opendbc_repo/opendbc/car/toyota/radar_interface.py
@@ -82,6 +82,13 @@ class RadarInterface(RadarInterfaceBase):
             self.pts[ii] = RadarData.RadarPoint()
             self.pts[ii].trackId = self.track_id
             self.track_id += 1
+          xinput = int(cpt['LONG_DIST'])
+          yinput = float(cpt['LAT_DIST'])
+          yprint = round(yinput,2)
+          yoffsetoutput = float(np.interp(xinput, xdistance, yoffset)) 
+          yOO = (round(yoffsetoutput,2))
+          #print(f"Long: {xinput} Lat: {yprint} Score: {score}")
+          cpt['LAT_DIST'] = cpt['LAT_DIST'] - yOO        # + shifts to the right
           self.pts[ii].dRel = cpt['LONG_DIST']  # from front of car
           self.pts[ii].yRel = -cpt['LAT_DIST']  # in car frame's y axis, left is positive
           self.pts[ii].vRel = cpt['REL_SPEED']
