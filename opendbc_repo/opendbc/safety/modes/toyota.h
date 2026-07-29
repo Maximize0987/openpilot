50f849148218b118e5486c4a733d285906ba6136
Maximize0987 <42393700+Maximize0987@users.noreply.github.com>
2026-07-29 14:22:57 -0600

    Update toyota.h    try lower driver torque hoping for better take over
    Signed-off-by: Maximize0987 <42393700+Maximize0987@users.noreply.github.com>

--- opendbc_repo/opendbc/safety/modes/toyota.h
+++ opendbc_repo/opendbc/safety/modes/toyota.h
@@ -254,7 +254,7 @@ static bool toyota_tx_hook(const CANPacket_t *msg) {
   };
 
   const int TOYOTA_LTA_MAX_MEAS_TORQUE = 1500;
-  const int TOYOTA_LTA_MAX_DRIVER_TORQUE = 150;
+  const int TOYOTA_LTA_MAX_DRIVER_TORQUE = 75; //const int TOYOTA_LTA_MAX_DRIVER_TORQUE = 150;
 
   // longitudinal limits
   const LongitudinalLimits TOYOTA_LONG_LIMITS = {
