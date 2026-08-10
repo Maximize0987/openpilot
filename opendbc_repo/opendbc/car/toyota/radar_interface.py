98ce12c3123a96ec7fae45522fed98fb1ca7f75d
Maximize0987 <42393700+Maximize0987@users.noreply.github.com>
2026-08-09 19:24:09 -0600

    Update radar_interface.py   more offset
    Signed-off-by: Maximize0987 <42393700+Maximize0987@users.noreply.github.com>

--- opendbc_repo/opendbc/car/toyota/radar_interface.py
+++ opendbc_repo/opendbc/car/toyota/radar_interface.py
@@ -12,7 +12,7 @@ KPH_TO_MS = 1. / 3.6
 TSSP_RADAR_EGO_SPEED_SCALE = 0.922
 
 xdistance = np.array([0, 230], dtype=np.float32)
-yoffset = np.array([0.0, 0.18], dtype=np.float32)
+yoffset = np.array([0.0, 0.2], dtype=np.float32)
 ydistin = np.array([-2.0, -0.5, 0.0, 0.5, 2], dtype=np.float32)
 ydistout = np.array([-3.0, -0.7, 0.0, 0.7, 3.0], dtype=np.float32)
 
