138d6bb0e8fa9cfc9c0dfcb6fd852b6a30a6768d
Maximize0987 <42393700+Maximize0987@users.noreply.github.com>
2026-08-09 19:30:04 -0600

    Update radar_interface.py  extend offset array more values
    Signed-off-by: Maximize0987 <42393700+Maximize0987@users.noreply.github.com>

--- opendbc_repo/opendbc/car/toyota/radar_interface.py
+++ opendbc_repo/opendbc/car/toyota/radar_interface.py
@@ -13,8 +13,8 @@ TSSP_RADAR_EGO_SPEED_SCALE = 0.922
 
 xdistance = np.array([0, 230], dtype=np.float32)
 yoffset = np.array([0.0, 0.2], dtype=np.float32)
-ydistin = np.array([-2.0, -0.5, 0.0, 0.5, 2], dtype=np.float32)
-ydistout = np.array([-3.0, -0.7, 0.0, 0.7, 3.0], dtype=np.float32)
+ydistin = np.array([-2.0, -0.5, , -0.3, 0.0, 0.3, 0.5, 2], dtype=np.float32)
+ydistout = np.array([-3.0, -0.7, -0.3, 0.0, , 0.3, 0.7, 3.0], dtype=np.float32)
 
 def _create_radar_can_parser(car_fingerprint):
   if car_fingerprint in TSS2_CAR:
