86e51863416629c5bba6d1bf136196e20f5092cb
firestar5683 <168790843+firestar5683@users.noreply.github.com>
2026-08-17 10:05:21 -0500

    Revert "optimize"
    This reverts commit b567d9fc909d5f8f5df54af67c87f8f8c6375a50.

--- common/time_helpers.py
+++ common/time_helpers.py
@@ -4,16 +4,13 @@ from pathlib import Path
 MIN_DATE = datetime.datetime(year=2025, month=2, day=21)
 MAX_DATE = datetime.datetime(year=2035, month=1, day=1)
 
-def _utc_now_naive():
-  return datetime.datetime.now(datetime.UTC).replace(tzinfo=None)
-
 def min_date():
   # on systemd systems, the default time is the systemd build time
   systemd_path = Path("/lib/systemd/systemd")
   if systemd_path.exists():
-    d = datetime.datetime.fromtimestamp(systemd_path.stat().st_mtime, datetime.UTC).replace(tzinfo=None)
+    d = datetime.datetime.fromtimestamp(systemd_path.stat().st_mtime)
     return max(MIN_DATE, d + datetime.timedelta(days=1))
   return MIN_DATE
 
 def system_time_valid():
-  return min_date() < _utc_now_naive() < MAX_DATE
+  return min_date() < datetime.datetime.now() < MAX_DATE
